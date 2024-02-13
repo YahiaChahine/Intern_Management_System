1e914e8f8bf14a2140b62098e69fe80177fe7587
Yahia Shahin <yahia.a.shahin@gmail.com>
2024-02-13 22:19:10 +0400

    Initial commit

--- InternsProgram/src/main/java/com/yahia/internsApplication/model/recruiter/Recruiter.java
+++ InternsProgram/src/main/java/com/yahia/internsApplication/model/recruiter/Recruiter.java
@@ -3,11 +3,10 @@ package com.yahia.internsApplication.model.recruiter;
 import com.yahia.internsApplication.model.Role;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+
 
 import java.util.Collection;
 import java.util.List;
@@ -16,7 +15,8 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Recruiter implements UserDetails {
+@Builder
+public class Recruiter {
     @Id
     @SequenceGenerator(
             name = "recruiter_id_sequence",
@@ -34,33 +34,6 @@ public class Recruiter implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
-    }
 
-    @Override
-    public String getUsername() {
-        return email;
-    }
 
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
